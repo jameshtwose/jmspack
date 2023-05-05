7cb9024a6ecafc35a414b1b9038ad7182339dc7c
James Twose <jamestwose@Mac-mini.home>
2023-05-05 15:47:21 +0200

    updating the CNAME reference in the conf.py of the docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -109,6 +109,7 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_extra_path = ["CNAME"]
 
 # Generate the API documentation when building
 autosummary_generate = True
